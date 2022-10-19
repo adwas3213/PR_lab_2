b77cbcec2236eaebda414126513d1409228a235b
Adam Wasylewicz <adwas33@wp.pl>
2022-10-19 19:19:59 +0200

    added fork.c solution

--- fork.c
+++ fork.c
@@ -2,33 +2,37 @@
 #include<stdio.h>
 #include<unistd.h>
 
-int zmienna_globalna=0;
+#include "pomiar_czasu/pomiar_czasu.h"
 
-main(){
-  
-  int pid, wynik, i;
-  
-  for(i=0;i<1000;i++){
-    
-    pid = fork(); 			
-    
-    if(pid==0){ 			
-      
-      zmienna_globalna++;
-      
-      /* wynik=execv("./program",NULL); */
-      /* if(wynik==-1) */
-      /*     printf("Proces potomny nie wykonal programu\n"); */
+int zmienna_globalna = 0;
 
-      exit(0);
-      
-    } else {					
-      
-//      wait(NULL);
-      
+main() {
+
+    int pid, wynik, i;
+
+    inicjuj_czas();
+    for (i = 0; i < 10000; i++) {
+
+        pid = fork();
+
+        if (pid == 0) {
+
+            zmienna_globalna++;
+
+            /* wynik=execv("./program",NULL); */
+            /* if(wynik==-1) */
+            /*     printf("Proces potomny nie wykonal programu\n"); */
+
+            exit(0);
+
+        } else {
+
+            wait(NULL);
+
+        }
     }
-  }
-  
+    drukuj_czas();
+
 }
 
   
